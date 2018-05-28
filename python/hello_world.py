d723c79eeb8dcde0c179faa3939f29b94e9469b9
tonnyLiu <39550234+tonnyLiu@users.noreply.github.com>
2018-05-28 17:19:00 +0800

    first

--- python/hello_world.py
+++ python/hello_world.py
@@ -1,3 +1,19 @@
-print("hello world")
-print("hello world")
-print("hello world")
+class Person():
+    def __init__(self):
+        print("这是构造函数")
+    #公有成员
+    name = None
+    #私有成员
+    __age = 24
+    def eat(self):
+        print("我需要吃饭")
+
+class Student(Person):
+    def __init__(self):
+        super().__init__()
+        print("我也有一个构造函数")
+
+liu = Student()
+liu.name = "刘帅"
+liu.eat()
+print("Hello World OOP!!!")
